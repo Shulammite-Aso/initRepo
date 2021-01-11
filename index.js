c693ba678d7a13109e76698412658317e14f6dd1
Shulammite-Aso <asoamarachi@gmail.com>
2021-01-11 13:42:54 +0100

    enable creating remote repo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+
 const files = require("./lib/files");
+//const github = require('./lib/github');
 
 clear();
 
@@ -15,3 +17,10 @@ if (files.directoryExists(".git")) {
     console.log(chalk.red("Already a git repository."));
     process.exit();
 }
+
+const run = async () => {
+    const credentials = await inquirer.requestGithubCredentials();
+    console.log(credentials);
+};
+
+run();
